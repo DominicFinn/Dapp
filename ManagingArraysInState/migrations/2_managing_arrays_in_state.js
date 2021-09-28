a134968ffdb6b74724b405296bd4b5d0948c5922
DomFinn <dom.finn@gmail.com>
2021-09-28 20:43:13 +0100

    Trying out array management

---
+++ ManagingArraysInState/migrations/2_managing_arrays_in_state.js
@@ -0,0 +1,5 @@
+const ManagingArraysInState = artifacts.require("ManagingArraysInState");
+
+module.exports = function (deployer) {
+  deployer.deploy(ManagingArraysInState);
+};
---
+++ ManagingArraysInState/test/managing_arrays_in_state.js
@@ -0,0 +1,13 @@
+const ManagingArraysInState = artifacts.require("ManagingArraysInState");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("ManagingArraysInState", function (/* accounts */) {
+  it("should ", async function () {
+    await ManagingArraysInState.deployed();
+    return assert.isTrue(true);
+  });
+});
